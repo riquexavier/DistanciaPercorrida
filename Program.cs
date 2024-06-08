6d6d67d0b52552571fb0cb6296359cd5a4722f6c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-07 22:41:26 -0300

    progress

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("|ORIGEM DESTINO | Belo Horizonte | Rio de janeiro | São Paulo | Vitória |");
+
+Console.WriteLine("|Belo horizonte |                |                |           |         |");
+
+Console.WriteLine("|Rio de Janeiro |                |                |           |         |");
+
+Console.WriteLine("|Sâo Paulo      |                |                |           |         |");
+
+Console.WriteLine("|Vitória        |                |                |           |         |");
