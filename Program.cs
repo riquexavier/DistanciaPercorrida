d473f9c062f27adb50dac2d2ef73b5aa2eb8df0c
Pedrox | Adm! Dev! <110746209+riquexavier@users.noreply.github.com>
2024-06-13 15:07:51 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,3 +8,15 @@ Console.WriteLine("|Rio de Janeiro |                |                |          
 Console.WriteLine("|Sâo Paulo      |                |                |           |         |");
 
 Console.WriteLine("|Vitória        |                |                |           |         |");
+
+Console.WriteLine("-------------------------------------------------------------------------");
+Console.WriteLine("|ORIGEM DESTINO | Belo Horizonte | Rio de Janeiro | São Paulo | Vitória |");
+Console.WriteLine("|---------------|----------------|----------------|-----------|---------|");
+Console.WriteLine("|Belo Horizonte |       0        |      434       |    586    |   524   |");
+Console.WriteLine("|---------------|----------------|----------------|-----------|---------|");
+Console.WriteLine("|Rio de Janeiro |      434       |       0        |    429    |   521   |");
+Console.WriteLine("|---------------|----------------|----------------|-----------|---------|");
+Console.WriteLine("|Sâo Paulo      |      586       |      429       |     0     |   882   |");
+Console.WriteLine("|---------------|----------------|----------------|-----------|---------|");
+Console.WriteLine("|Vitória        |      524       |      521       |    882    |    0    |");
+Console.WriteLine("-------------------------------------------------------------------------");
